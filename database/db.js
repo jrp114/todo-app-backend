1df6958b3b4ab726334185845402b21958f6df83
Jeremy Price <jrp114@yahoo.com>
2023-09-11 12:23:39 -0400

    update2

--- database/db.js
+++ database/db.js
@@ -11,12 +11,20 @@ const initOptions = {
 
 const pgp = PgPromise(initOptions);
 
+const ssl =
+  process.env.NODE_ENV === 'heroku-dev'
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false;
+
 const dbConfig = {
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_NAME,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
+  ssl,
 };
 
 const db = pgp(dbConfig);
