7170249b25f3594707858195ede87ae3076bdeaf
Jeremy Price <jrp114@yahoo.com>
2024-02-12 12:13:34 -0500

    fix get comment route

--- src/routes/comments/service.ts
+++ src/routes/comments/service.ts
@@ -2,11 +2,8 @@ import { Request, Response } from 'express';
 import db from '../../database/db';
 
 export async function getComment(req: Request, res: Response) {
-  const comment = await db.comments.updateComment({
-    ...req.body,
-    id: req.params.id,
-  });
-  res.status(201).send(comment);
+  const result = await db.comments.getByTodo(req.query.todoId);
+  res.status(200).send(result);
 }
 
 export async function postComment(req: Request, res: Response) {
